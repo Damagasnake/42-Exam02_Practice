010a436135f763c891492ab0107c891f774601a3
Damaga <damagasnake@gmail.com>
2025-04-22 09:32:16 +0200

    mod. write fizzbuzz

--- Level01/fizzbuzz.c
+++ Level01/fizzbuzz.c
@@ -14,7 +14,7 @@ int main(void)
     while(n <= 100)
     {
     if (n % 15 == 0)
-        write(1, "fizzbuzz", 1);
+        write(1, "fizzbuzz", 8);
     else if (n % 3 == 0)
         write(1, "fizz", 4);
     else if (n % 5 == 0)
